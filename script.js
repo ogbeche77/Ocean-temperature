a0edd96cc11c3b90cbc38c1c9f07509f3b97d54c
ogbeche77 <ogbeche77@yahoo.com>
2020-02-07 10:59:22 +0100

    split

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const response = await fetch ("GLB.Ts+dSST.csv");
 const data = await response.text();
 
 
-
+// data first seperated with line breaks
 const table = data.split("\n");
 console.log(table);
 table.forEach(columns =>{
     const row = columns.split(",")
+    //showing the average ocean temp in the first 4 rows (years, jan,feb & march)
     const year = row[0];
     const month1 = row[1];
     const month2 = row[2];
